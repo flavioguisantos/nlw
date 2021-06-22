30263948c950b5db83a6088b0bb777ec9af7d4ad
flavioguisantos <flavio.guilherme@outlook.com.br>
2021-06-22 00:01:30 -0300

    Segundo dia

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,11 @@
-import express, { request, response } from "express"
+import "reflect-metadata"
+import express from 'express'
+import { router } from "./routes"
+import "./database"
+
 
 const app = express()
+app.use(express.json())
+app.use(router)
 
-/**
- * GET    => Buscar uma informação
- * POST   => Inserir (Criar) uma informação
- * PUT    => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação especifica 
- */
-
-app.get("/test", (request, response) => {
-  // Request  => Entrando
-  // Response => Saindo
-return response.send("Olá NLW")
-})
-
-app.post("test-post",(request, response) => {
-  return response.send("Olá NLW método POST")
-})
-app.listen(3000, () => console.log("Server is running NLW"))
+app.listen(3000, () => console.log('Server is running NLW'))
